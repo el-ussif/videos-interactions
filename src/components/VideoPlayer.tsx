423dc99cd4807a990ac38e218a2aedfd3ac55c26
Youssifou Awanou <youssifou.awanou@gmail.com>
2025-07-15 23:02:07 +0000

    work on video preload

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -321,7 +321,7 @@ export default function VideoPlayer() {
                         onClick={handleUserStart}
                         className="mt-4 text-white px-4 py-2 bg-blue-600 rounded-full"
                     >
-                        Démarrer
+                        Start
                     </button>
                 </div>
             )}
